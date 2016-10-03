8f249ce8124381e28a94c4805eec3b018e9ced70
Leonardo Pereira <leonardo.pereira@intranet.agres>
2016-10-03 09:17:27 -0300

    alterado filosofo.c e .h

--- 05_Filosofos/filosofo.c
+++ 05_Filosofos/filosofo.c
@@ -1,58 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
 #include "filosofo.h"
+
+#define DELAY 5
+
+int status[5];
+int nfilosofo[5] = {0, 1, 2, 3, 4};
+
+sem_t sem_mutex;
+sem_t S[5];
 
 void *filosofo(void *num)
 {
-    while(1)
+    for (;;)
     {
-        int *i = num;
-        
-        pegaTalher(*i);
-        largaTalher(*i);
-        
-        Sleep(DELAY);
+        int *i = num;
+
+        pegaTalher(*i);
+        largaTalher(*i);
+
+        sleep(DELAY);
     }
 }
 
 void pegaTalher(int nfilosofo)
 {
-    sem_wait(&sem_mutex);
+    sem_wait(&sem_mutex);
 
-    status[nfilosofo] = 1;
+    status[nfilosofo] = 1;
 
-    printf("Filosofo com fome: %d \n", nfilosofo+1);
+    printf("Filosofo com fome: %d \n", nfilosofo + 1);
 
-    checar(nfilosofo);
+    checar(nfilosofo);
 
-    sem_post(&sem_mutex);
-    sem_wait(&S[nfilosofo]);
+    sem_post(&sem_mutex);
+
+    sem_wait(&S[nfilosofo]);
 }
 
 void largaTalher(int nfilosofo)
 {
-    sem_wait(&sem_mutex);
-    
-    status[nfilosofo]=0;
-    
-    printf("Filosofo n. %d largou os Talhers %d e %d.\n", nfilosofo+1, ESQ+1, nfilosofo+1);
-    printf("Filosofo pensando :%d \n", nfilosofo+1);
-    
-    checar(ESQ);
-    checar(DIR);
-    
-    sem_post(&sem_mutex);
+    sem_wait(&sem_mutex);
+
+    status[nfilosofo] = 0;
+
+    printf("Filosofo %d largou os talheres %d e %d.\n", nfilosofo + 1, ESQUERDA + 1, nfilosofo + 1);
+
+    printf("Filosofo %d pensando...\n", nfilosofo + 1);
+
+    checar(ESQUERDA);
+
+    checar(DIREITA);
+
+    sem_post(&sem_mutex);
 }
 
 void checar(int nfilosofo)
 {
-    if(status[nfilosofo] == 1 && status[ESQ] != 2 && status[DIR] != 2)
+    if ((status[nfilosofo] == 1) && (status[ESQUERDA] != 2) && (status[DIREITA] != 2))
     {
-        status[nfilosofo] = 2;
-        
-        Sleep(2 * DELAY);
-        
-        printf("Filosofo n. %d pegou os Talhers %d e %d.\n", nfilosofo+1, ESQ+1, nfilosofo+1);
-        printf("Filosofo comendo: %d \n", nfilosofo+1);
-        
-        sem_post(&S[nfilosofo]);
+        status[nfilosofo] = 2;
+
+        sleep(2 * DELAY);
+
+        printf("Filosofo %d pegou os talheres %d e %d.\n", nfilosofo + 1, ESQUERDA + 1, nfilosofo + 1);
+
+        printf("Filosofo %d comendo...\n", nfilosofo + 1);
+
+        sem_post(&S[nfilosofo]);
     }
 }
--- 05_Filosofos/filosofo.h
+++ 05_Filosofos/filosofo.h
@@ -1,3 +1,17 @@
+#ifndef __FILOSOFO__
+#define __FILOSOFO__
+
+#include <semaphore.h>
+
+#define ESQUERDA (nfilosofo + 4) % 5
+#define DIREITA  (nfilosofo + 1) % 5
+
+extern int status[5];
+extern int nfilosofo[5];
+
+extern sem_t sem_mutex;
+extern sem_t S[5];
+
 void *filosofo(void *num);
 
 void pegaTalher(int nfilosofo);
@@ -5,3 +19,5 @@ void pegaTalher(int nfilosofo);
 void largaTalher(int nfilosofo);
 
 void checar(int nfilosofo);
+
+#endif
